8b1f11dba7b44bfb00f7ff7e1341baeef3c11433
Adam Sitabkhan <adam.sitabkhan@gmail.com>
2025-07-08 23:04:30 -0500

    Reads .zip file provided by Spotify, displays top 10 tracks

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
+
 import pandas as pd
+import os
+import zipfile
+import tempfile
+import json
+import time
 import io
 
 app = FastAPI()
+
+def print_tree(start_path, prefix=""):
+    for item in os.listdir(start_path):
+        path = os.path.join(start_path, item)
+        print(prefix + "├── " + item)
+        if os.path.isdir(path):
+            print_tree(path, prefix + "│   ")
 
 # Allow React frontend to communicate with FastAPI
 app.add_middleware(
@@ -21,16 +34,43 @@ def root():
 
 @app.post("/upload")
 async def upload_csv(file: UploadFile = File(...)):
-    if not file.filename.endswith(".csv"):
-        return JSONResponse(status_code=400, content={"error": "Only CSV files are supported."})
-
+    if not file.filename.endswith(".zip"):
+        return JSONResponse(status_code=400, content={"error": "Please provide the .zip file sent to you by Spotify."})
+    
+    filename = file.filename.lower()
     content = await file.read()
-    df = pd.read_csv(io.BytesIO(content))
+    
+    start_time = time.perf_counter()
+    with tempfile.TemporaryDirectory() as tmpdir:
+        zip_path = os.path.join(tmpdir, filename)
+        with open(zip_path, "wb") as f:
+            f.write(content)
+
+        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+            zip_ref.extractall(tmpdir)
+        
+        datadir = f"{tmpdir}\\Spotify Extended Streaming History"
+    
+        full_df = None
+        for datafilename in os.listdir(datadir):
+            datafilepath = os.path.join(datadir, datafilename)
+            
+            if not datafilepath.endswith(".json"): continue
+                
+            with open(datafilepath, "r", encoding="utf-8") as json_file:
+                data = json.load(json_file)
+                df = pd.DataFrame(data)
+                if full_df is None:
+                    full_df = df
+                else:
+                    full_df = pd.concat([full_df, df], ignore_index=True)
+    end_time = time.perf_counter()
+    print("Time:", end_time - start_time)
 
     # Simplified assumption: the CSV has a "trackName" column
-    if "trackName" not in df.columns:
-        return JSONResponse(status_code=400, content={"error": "Missing 'trackName' column in uploaded CSV."})
+    if "master_metadata_track_name" not in full_df.columns:
+        return JSONResponse(status_code=400, content={"error": "Missing 'master_metadata_track_name' column in one of uploaded JSON files."})
 
-    top_tracks = df["trackName"].value_counts().head(5).to_dict()
+    top_tracks = full_df["master_metadata_track_name"].value_counts().head(10).to_dict()
 
     return {"top_tracks": top_tracks}
